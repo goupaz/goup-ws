cfcaed2d64169a300138bf12c4ecf7fdce549ea9
Sako <sako@opengov.com>
2021-05-30 23:47:53 -0700

    changes

--- lib/constants.ts
+++ lib/constants.ts
@@ -17,14 +17,14 @@
 export const SITE_URL = 'https://demo.vercel.events';
 export const SITE_ORIGIN = process.env.NEXT_PUBLIC_SITE_ORIGIN || new URL(SITE_URL).origin;
 export const TWITTER_USER_NAME = 'vercel';
-export const BRAND_NAME = 'ACME';
-export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
-export const SITE_NAME = 'ACME Conf';
+export const BRAND_NAME = 'GOUP';
+export const SITE_NAME_MULTILINE = ['GOUP', 'Conf'];
+export const SITE_NAME = 'GOUP Conf';
 export const META_DESCRIPTION =
   'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
 export const SITE_DESCRIPTION =
   'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
+export const DATE = 'June 04, 2021';
 export const SHORT_DATE = 'Oct 27 - 9:00am PST';
 export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
 export const TWEET_TEXT = META_DESCRIPTION;
@@ -41,35 +41,36 @@ export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
-    name: 'Stage A',
+    name: 'Home',
     route: '/stage/a'
   },
   {
-    name: 'Stage C',
+    name: 'Techbrains',
     route: '/stage/c'
   },
   {
-    name: 'Stage M',
+    name: 'kiss.conf',
     route: '/stage/m'
   },
   {
-    name: 'Stage E',
-    route: '/stage/e'
-  },
-  {
-    name: 'Schedule',
+    name: '02hero',
     route: '/schedule'
   },
+  {
+    name: 'Blog',
+    route: '/expo'
+  },
+
   {
     name: 'Speakers',
     route: '/speakers'
   },
   {
-    name: 'Expo',
+    name: 'News',
     route: '/expo'
   },
   {
-    name: 'Jobs',
+    name: 'About Us',
     route: '/jobs'
   }
 ];
